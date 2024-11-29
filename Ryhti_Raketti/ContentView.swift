1608d8a3c2f4082b6771ac318415010e851bec4f
Liuwei Luo <qian_27@hotmail.com>
2024-11-29 12:34:25 +0200

    First Commit

---
+++ Ryhti_Raketti/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ryhti_Raketti
+//
+//  Created by Lucky on 29.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ryhti_Raketti/Ryhti_RakettiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ryhti_RakettiApp.swift
+//  Ryhti_Raketti
+//
+//  Created by Lucky on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ryhti_RakettiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
